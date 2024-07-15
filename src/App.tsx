e12f9864df3d9b229ec6e583c94065fb8027a0be
skyInWater <coderhy@qq.com>
2024-07-15 09:30:15 +0800

    init: init project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h2>APP HOME</h2>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const rootSrc = path.resolve(__dirname, 'src')
+
+export default ({ mode = '' }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    base: env.VITE_APP_ENV === 'production' ? './' : './',
+    plugins: [react()],
+    resolve: { alias: { '@': rootSrc } }
+  })
+}
